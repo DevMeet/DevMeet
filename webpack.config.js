c697b92f6797129689196ebcc61f47e330106e39
Ryan Kim <tkdryan@hotmail.com>
2020-02-19 10:41:11 -0800

    boiler plate

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+//   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+    hot: true,
+    publicPath: '/build/',
+    // proxy: {
+    //   '/':
+    //   {
+    //     target: 'http://localhost:8080',
+    //     secure: false
+    //   }
+    // },
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Code Compete',
+      template: './src/index.html'
+    })
+  ],
+  mode: process.env.NODE_ENV,
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(css)|(scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|otf|jpg|gif)$/,
+        use: ['url-loader?limit=10000']
+      },
+    ]
+  }
+};
