94d8872c8fd4964b19c2b10454b647467217a469
Nancy Dao <nancyddao@gmail.com>
2020-02-20 15:57:03 -0800

    styling for event component

--- src/components/SingleEvent.jsx
+++ src/components/SingleEvent.jsx
@@ -9,8 +9,19 @@ class SingleEvent extends Component {
   render() {
     const newDate = moment(this.props.date).format('MMMM D, Y')
     return (
-      <div>
-        {this.props.name}
+      <div className="event-box">
+        <h1>{this.props.name}</h1>
+        <div style={{ display: 'flex' }}>
+          <div>
+            <h2>Venue: {this.props.venue}</h2>
+            <h2>{this.props.city}</h2>
+          </div>
+          <div>
+            <h3>{newDate}</h3>
+            <h4>Description: {this.props.description}</h4>
+            <h4><a href={this.props.url}>Visit Website</a></h4>
+          </div>
+        </div>
       </div>
     )
   }
