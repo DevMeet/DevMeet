4524992836c754cd427ec8e8d6c7a1ea4fce542f
Ryan Kim <tkdryan@hotmail.com>
2020-02-20 11:54:06 -0800

    working on routing

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import MapDisplay from '../components/MapDisplay.jsx'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap'
 import LocationDropDown from '../components/LocationDropDown.jsx';
+import Profile from '../components/Profile'
+import MyEvents from '../components/MyEvents'
+import TrendingEvents from '../components/TrendingEvents'
 
 class MainPage extends Component {
   constructor (props) {
@@ -16,6 +20,20 @@ class MainPage extends Component {
               <div className="googlemaps">
                   <MapDisplay/>
                   <LocationDropDown/>
+                  <Switch>
+                    <Route path="/profile">
+                      <Profile />
+                    </Route>
+                    <Route path="/events">
+                      <MyEvents />
+                    </Route>
+                    <Route path="/trending">
+                      <TrendingEvents />
+                    </Route>
+                    <Route path="/upcoming">
+                      <UpcomingEvents />
+                    </Route>
+                  </Switch>
               </div>
           </div>
       </div>
