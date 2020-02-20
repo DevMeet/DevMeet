49e85ebe20d70b2092e7907cabe5f87e292ea56a
Jason Jones <jasonroyjones@gmail.com>
2020-02-19 22:40:38 -0800

    pulled and resolved conflicts

--- src/components/MapDisplay.jsx
+++ src/components/MapDisplay.jsx
@@ -1,43 +1,35 @@
 //this is a react component wow
 
 import React, { Component } from 'react';
-import {
-    withGoogleMap,
-    withScriptjs,
-    GoogleMap,
-    Marker,
-    InfoWindow
-  } from "react-google-maps";
+import { withGoogleMap, withScriptjs, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 
 // const AnyReactComponent = ({ text }) => <div>{text}</div>;
 class MapDisplay extends Component {
   constructor(props) {
-      super(props);
+    super(props);
   }
   render() {
     function Map() {
       return (
-          <GoogleMap
-          defaultZoom={8}
-          defaultCenter={{lat: 34.052235, lng: -118.243683}}
-          >
-          </GoogleMap>
-      )
-  }
-  
-  const MapWrapped = withScriptjs(withGoogleMap(Map));
-      return (
-        <div>
-          <div style={{ width: '60vw' , height: '40vh'}}>
-              <MapWrapped 
-              googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyABc2ZRJ4eqDNoO7r_qGSiHwr5drpWYADM'}
-              loadingElement={<div style={{height: '100%'}} /> }
-              containerElement={<div style={{height: '100%'}} /> }
-              mapElement={<div style={{height: '100%'}} /> }
-              />
-          </div>
-        </div>   
-      )
+        <GoogleMap defaultZoom={8} defaultCenter={{ lat: 34.052235, lng: -118.243683 }}></GoogleMap>
+      );
+    }
+
+    const MapWrapped = withScriptjs(withGoogleMap(Map));
+    return (
+      <div>
+        <div style={{ width: '60vw', height: '40vh' }}>
+          <MapWrapped
+            googleMapURL={
+              'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyABc2ZRJ4eqDNoO7r_qGSiHwr5drpWYADM'
+            }
+            loadingElement={<div style={{ height: '100%' }} />}
+            containerElement={<div style={{ height: '100%' }} />}
+            mapElement={<div style={{ height: '100%' }} />}
+          />
+        </div>
+      </div>
+    );
   }
 }
 export default MapDisplay;
